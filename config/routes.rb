a516e1efff35d020f2a8a899641e6c8b9b7b05b5
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 19:51:33 -0400

    Routes finally close to working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do |map|
-  map.match "managed_emails", :to => "managed_emails#index", :as => "email_manager_managed_emails"
-  map.match "managed_email/:id", :to => "managed_emails#show", :as => "email_manager_managed_email", :method => :get
-  map.match "managed_email/:id/body", :to => "managed_emails#body", :as => "email_managed_managed_email_body", :method => :get
+Rails.application.routes.draw do
+  namespace :email_manager do
+    match "managed_emails", :to => "managed_emails#index", :as => "email_manager_managed_emails"
+    match "managed_email/:id", :to => "managed_emails#show", :as => "email_manager_managed_email", :method => :get
+    match "managed_email/:id/body", :to => "managed_emails#body", :as => "email_managed_managed_email_body", :method => :get
+  end
 end
