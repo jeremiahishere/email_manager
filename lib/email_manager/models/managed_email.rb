e926e20677389da31e2f3f640d730ff2e3627764
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 12:06:00 -0400

    Version bump to 0.0.1

---
+++ lib/email_manager/models/managed_email.rb
@@ -0,0 +1,4 @@
+# Managed Email stores the recorded emails information.
+# This includes the subject, body, from, to, reply-to, and calling file
+class ManagedEmail < ActiveRecord::Base
+end
---
+++ lib/generators/email_manager/config_generator.rb
@@ -0,0 +1,16 @@
+module EmailManager
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      source_root File.expane("../templates", __FILE__)
+
+      desc <<DESC
+Description:
+  Copies the Email Manager configuration file to the user's initializer directory.
+DESC
+      
+      def copy_config_file
+        template "email_manager_config.rb", "config/initializers/email_manager_config.rb"
+      end
+    end
+  end
+end
