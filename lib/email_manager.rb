5e2c70ed861e1b2cb5809ef2dd5dc73a5901eff1
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 15:56:03 -0400

    Changing file structure

--- lib/email_manager.rb
+++ lib/email_manager.rb
@@ -2,4 +2,4 @@
 # now accessible as EmailManager::ManagedEmail
 require File.join(%w{email_manager managed_email})
 # make the observer accessible as EmailManager::ManagedEmailObserver
-require File.join(%w{email_manager email_observer})
+require File.join(%w{email_manager managed_email_observer})
