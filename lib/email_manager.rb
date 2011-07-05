fa6c8246ce4863709a3211bbb46764eb9544798b
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 15:44:33 -0400

    Added the email observer

--- lib/email_manager.rb
+++ lib/email_manager.rb
@@ -1,2 +1,5 @@
-require File.join(%w{email_manager managed_email})
 # Add the email manager to the project
+# now accessible as EmailManager::ManagedEmail
+require File.join(%w{email_manager managed_email})
+# make the observer accessible as EmailManager::ManagedEmailObserver
+require File.join(%w{email_manager email_observer})
