b6e1daad9dbc8e691bbf3bc6697192e6c62b6c72
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 19:38:25 -0400

    Namespaced the controller and made it unloadable

--- app/controllers/managed_emails_controller.rb
+++ app/controllers/managed_emails_controller.rb
@@ -2,46 +2,49 @@
 # Allows users to search for and view managed emails but does not allow editing or creation
 # Includes optional support for meta_search and kaminari gems
 # @author Jeremiah Hemphill
-class ManagedEmailsController < ApplicationController
+module EmailManager
+  class ManagedEmailsController < ApplicationController
+    unloadable
 
-  # shows all managed emails with pagination and sorting
-  def index
-    # Search with meta_search
-    if EmailManager::ManagedEmail.respond_to?(:search)
-      @search = EmailManager::ManagedEmail.search(params[:search])
-      @search_enabled = true
-    else
-      @search = EmailManaged::ManagedEmail.all
+    # shows all managed emails with pagination and sorting
+    def index
+      # Search with meta_search
+      if EmailManager::ManagedEmail.respond_to?(:search)
+        @search = EmailManager::ManagedEmail.search(params[:search])
+        @search_enabled = true
+      else
+        @search = EmailManaged::ManagedEmail.all
+      end 
+
+      # pagination with kaminari
+      begin
+        @managed_emails = Kaminari.paginate_array(@search.all).page(params[:page])
+        @pagination_enabled = true
+      rescue LoadError => e
+        @managed_emails = @search
+      end 
+
+      respond_to do |format|
+        format.html
+      end 
     end 
 
-    # pagination with kaminari
-    begin
-      @managed_emails = Kaminari.paginate_array(@search.all).page(params[:page])
-      @pagination_enabled = true
-    rescue LoadError => e
-      @managed_emails = @search
+    # shows a single managed email
+    # @param [String] id The id of the selected managed email
+    def show
+      @managed_email = ManagedEmail.find_by_id(params[:id])
+
+      respond_to do |format|
+        format.html
+      end 
     end 
 
-    respond_to do |format|
-      format.html
+    def body
+      @managed_email = ManagedEmail.find_by_id(params[:id])
+
+      respond_to do |format|
+        format.html { render :text => @managed_email.body }
+      end 
     end 
-  end 
-
-  # shows a single managed email
-  # @param [String] id The id of the selected managed email
-  def show
-    @managed_email = ManagedEmail.find_by_id(params[:id])
-
-    respond_to do |format|
-      format.html
-    end 
-  end 
-
-  def body
-    @managed_email = ManagedEmail.find_by_id(params[:id])
-
-    respond_to do |format|
-      format.html { render :text => @managed_email.body }
-    end 
-  end 
+  end
 end
