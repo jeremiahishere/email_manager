12750bfd7d0009876c79bb73da9e3b79a43766c3
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 19:00:24 -0400

    Added initial support for controller, views, and routes.  No idea if it is working.

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+if Rails.respond_to?(:application)
+  Rails.application.routes.draw do
+    match "managed_emails", :to => "managed_emails#index", :as => "email_manager_managed_emails"
+    match "managed_email/:id", :to => "managed_emails#show", :as => "email_manager_managed_email", :method => :get
+    match "managed_email/:id/body", :to => "managed_emails#body", :as => "email_managed_managed_email_body", :method => :get
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.match "managed_emails", :to => "managed_emails#index", :as => "email_manager_managed_emails"
+    map.match "managed_email/:id", :to => "managed_emails#show", :as => "email_manager_managed_email", :method => :get
+    map.match "managed_email/:id/body", :to => "managed_emails#body", :as => "email_managed_managed_email_body", :method => :get
+  end
+end
