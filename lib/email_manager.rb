26224ba74c476f150fc711357e750046f6be3e11
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 19:47:07 -0400

    Added a engine require to the email_manager.rb file

--- lib/email_manager.rb
+++ lib/email_manager.rb
@@ -3,3 +3,5 @@
 require File.join(%w{email_manager managed_email})
 # make the observer accessible as EmailManager::ManagedEmailObserver
 require File.join(%w{email_manager managed_email_observer})
+
+require "email_manager/engine"
