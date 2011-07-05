ee8b79ce6db40b8829096ade32313b7866d90769
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 17:43:49 -0400

    More testing with kaminari and meta where

--- lib/email_manager/managed_email.rb
+++ lib/email_manager/managed_email.rb
@@ -1,5 +1,7 @@
 # Managed Email stores the recorded emails information.
 # This includes the subject, body, from, to, reply-to, and calling file
+require "kaminari"
+require "meta_where"
 module EmailManager
   class ManagedEmail < ActiveRecord::Base
   end
