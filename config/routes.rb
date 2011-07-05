50023393f6a434b85de8868a238b724f81e3286a
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-05 19:21:03 -0400

    Setting up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 if Rails.respond_to?(:application)
+  puts "hello routes"
   Rails.application.routes.draw do
     match "managed_emails", :to => "managed_emails#index", :as => "email_manager_managed_emails"
     match "managed_email/:id", :to => "managed_emails#show", :as => "email_manager_managed_email", :method => :get
