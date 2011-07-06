d51e505d155d02c32d67b1509962998c080ac618
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-06 12:48:26 -0400

    Trying to get routing tests working

--- spec/routing/managed_emails_spec.rb
+++ spec/routing/managed_emails_spec.rb
@@ -3,17 +3,14 @@ require 'spec_helper'
 describe EmailManager::ManagedEmailsController do
   describe "routes" do
     it "recognizes and generates #index" do
-      pending
       { :get => "/email_manager/managed_emails" }.should route_to(:controller => "email_manager/managed_emails", :action => :index)
     end
 
     it "recognizes and generates #show" do
-      pending
-      { :get => "/email_manager/managed_email/1" }.should route_to(:controller => "email_manager/managed_emails", :action => :show, :id => 1)
+      { :get => "/email_manager/managed_email/1" }.should route_to(:controller => "managed_emails", :action => :show, :id => 1)
     end
 
     it "recognizes and generates #body" do
-      pending
       { :get => "/email_manager/managed_email/1/body" }.should route_to(:controller => "email_manager/managed_emails", :action => :body, :id => 1)
     end
   end
