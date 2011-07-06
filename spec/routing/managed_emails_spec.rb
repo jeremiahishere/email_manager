35d1f94dad3a93073ac67876b305c95d359cc106
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-06 13:02:52 -0400

    Finished routing tests

--- spec/routing/managed_emails_spec.rb
+++ spec/routing/managed_emails_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe EmailManager::ManagedEmailsController do
   describe "routes" do
     it "recognizes and generates #index" do
-      { :get => "/email_manager/managed_emails" }.should route_to(:controller => "email_manager/managed_emails", :action => :index)
+      email_manager_managed_emails_path.should == "/email_manager/managed_emails"
     end
 
     it "recognizes and generates #show" do
-      { :get => "/email_manager/managed_email/1" }.should route_to(:controller => "managed_emails", :action => :show, :id => 1)
+      email_manager_managed_email_path(1).should == "/email_manager/managed_email/1"
     end
 
     it "recognizes and generates #body" do
-      { :get => "/email_manager/managed_email/1/body" }.should route_to(:controller => "email_manager/managed_emails", :action => :body, :id => 1)
+      email_manager_managed_email_body_path(1).should == "/email_manager/managed_email/1/body"
     end
   end
 end
