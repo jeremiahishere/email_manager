c78e22cf810ffae0610eaea1cfe60eb3bacc9250
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-06 12:29:53 -0400

    Observer tests done for now.  Also had to rewrite the observer.

--- lib/email_manager/managed_email_observer.rb
+++ lib/email_manager/managed_email_observer.rb
@@ -6,22 +6,21 @@ module EmailManager
     # @param [Mail] mail The mail object that was just delivered
     def self.delivered_email(mail)
       # assume the caller is in the app folder somewhere
+
+      attributes = {}
+      %w{from reply_to to cc bcc}.each do |attribute|
+        attributes[attribute.to_sym] = mail.send(attribute.to_sym).nil? ? "" : mail.send(attribute.to_sym).join(", ")
+      end
+      %w{message_id subject body headers}.each do |attribute|
+        attributes[attribute.to_sym] = mail.send(attribute.to_sym).to_s
+      end
+      attributes[:date_sent] = mail.date
       calling_files = caller(0).select{ |val| val.match(Rails.root.join('app').to_s) }
-      calling_file_list = calling_files.join("\n")
+      attributes[:caller] = calling_files.join("\n")
+
 
       # create the managed email record
-      EmailManager::ManagedEmail.create(
-        :date_sent => mail.date,
-        :from => mail.from.join(", "),
-        :reply_to => mail.reply_to.join(", "),
-        :to => mail.to.join(", "),
-        :cc => mail.cc.join(", "),
-        :bcc => mail.bcc.join(", "),
-        :headers => mail.headers,
-        :message_id => mail.message_id,
-        :subject => mail.subject,
-        :body => mail.body.to_s,
-        :caller => calling_file_list)
+      EmailManager::ManagedEmail.create(attributes)
     end 
   end
 end
--- spec/managed_email_observer_spec.rb
+++ spec/managed_email_observer_spec.rb
@@ -3,22 +3,19 @@ require 'spec_helper'
 describe EmailManager::ManagedEmailObserver do
   describe "deliver_email" do
     before(:each) do
-      DummyMailer.dummy_email_notification
+      DummyMailer.deliver_dummy_email_notification
     end
 
-    it "should call self.delivered_email when an email is sent" do
-      EmailManager::ManagedEmailObserver.should_receive(:delivered_email)
-    end
-
-    it "should find the calling file of the email iff it is in /app" do
-      pending
+    it "should only store the calling file of the email if it is in /app" do
+      email = EmailManager::ManagedEmail.last
+      email.caller.should == ""
     end
 
     it "should create a ManagedEmail instance" do
       email = EmailManager::ManagedEmail.last 
       email.to.should == "test@email_manager.gem"
       email.subject.should == "Dummy email notification"
-      email.body.should == "This is a dummy email notification.  It should never be sent."
+      email.body.should == "This is a dummy email notification.  It should never be sent.\n"
     end
   end 
 end
