32fa30b6ee057def3a8f5c8be355ca63e059dc97
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-06 12:50:20 -0400

    Removed managed email model spec tests.  Because it doesn't do anything.

--- spec/models/managed_email_spec.rb
+++ spec/models/managed_email_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe EmailManager::ManagedEmail do
-  describe "validations" do
-    # it should always be valid
-    it "should be valid" do
-      pending
-    end
-  end
-  # it doesn't do much else
+  # it doesn't do much
 end
